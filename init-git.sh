6fc564e1dcb382f239e55792a566f5b6ccd809a5
Par <pacroy@gmail.com>
2020-12-29 18:05:25 +0700

    Adding message for git config

--- init-git.sh
+++ init-git.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 set -e
 
-echo "Configure git..."
 if [ -z "$(git config --global user.name)" ]; then 
-    read -p "Your name : " git_name </dev/tty
+    read -p "Your git name : " git_name </dev/tty
     git config --global user.name "$git_name"
 fi
 
 if [ -z "$(git config --global user.email)" ]; then
-    read -p "Your email: " git_email </dev/tty
+    read -p "Your git email: " git_email </dev/tty
     git config --global user.email $git_email
 fi
 
 if [ -z "$(git config --global push.default)" ]; then
+    echo "Setting git push.default=simple ..."
     git config --global push.default simple
 fi
 
 if [ -z "$(git config --global pull.rebase)" ]; then
+    echo "Setting git pull.rebase=false ..."
     git config --global pull.rebase false
 fi
