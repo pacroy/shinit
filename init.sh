f4af500afbd456c7c6ed57407ead4aa4e1cebb77
Par <pacroy@gmail.com>
2020-12-29 14:24:34 +0700

    Change to git repo model

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+INITDIR="$HOME/.init"
+INITSH="$INITDIR/init.sh"
+ALIASES="$INITDIR/aliases.sh"
+BINDIR="$HOME/bin"
+
+echo '#!/bin/sh' > "$INITSH"
+
+# Add aliases
+if [ -f "$ALIASES" ]; then
+    echo "source $ALIASES" >> "$INITSH"
+fi
+
+# Add bin directory to PATH
+if [ -d "$BINDIR" ]; then
+    echo 'PATH=$PATH:'"$BINDIR" >> "$INITSH"
+fi
+
+# Configure less behavior
+echo 'unset LESS' >> "$INITSH"
+
+# Configure .zshrc
+if [ -f "$HOME/.zshrc" ]; then
+    if ! cat $HOME/.zshrc | grep "source $INITSH" > /dev/null; then
+        echo "\nsource $INITSH" >> $HOME/.zshrc
+    fi
+fi
---
+++ install.sh
@@ -0,0 +1,7 @@
+!#/bin/sh
+INITDIR="$HOME/.init"
+
+git --version
+git clone --depth 1 https://github.com/pacroy/sh-init.git "$INITDIR"
+chmod +x "$INITDIR/install.sh"
+$INITDIR/install.sh
