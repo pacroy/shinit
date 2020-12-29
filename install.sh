e581bca6b25e846642d1e96ecf3f98256e287616
Par <pacroy@gmail.com>
2020-12-29 17:52:37 +0700

    Pull quietly

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ git --version
 if [ -d "$INITDIR" ]; then
     cd "$INITDIR"
     git reset --hard main
-    git pull
+    git pull --quiet
     cd $OLDPWD
 else
     git clone --depth 1 https://github.com/pacroy/sh-init.git "$INITDIR"
