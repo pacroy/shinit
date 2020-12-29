061e3e01ac3c901106cd7c6aead9f76a1817f4e7
Par <pacroy@gmail.com>
2020-12-29 15:54:32 +0700

    Use OLDPWD

--- install.sh
+++ install.sh
@@ -5,12 +5,10 @@ INITDIR="$HOME/.init"
 
 git --version
 if [ -d "$INITDIR" ]; then
-    CURRENTDIR=$(pwd)
     cd "$INITDIR"
     git reset --hard main
     git pull
-    cd $CURRENTDIR
-    unset CURRENTDIR
+    cd $OLDPWD
 else
     git clone --depth 1 https://github.com/pacroy/sh-init.git "$INITDIR"
 fi
