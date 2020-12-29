6a43d05ea2fd4718d2eae944a843ac2a3f6cdb49
Par <pacroy@gmail.com>
2020-12-29 16:25:24 +0700

    Rename install- to init-

--- install2.sh
+++ install2.sh
@@ -4,5 +4,5 @@ set -e
 INITDIR="$1"
 if [ -z $INITDIR ]; then echo 'INITDIR is not specified' >&2 && exit 1; fi
 
-$INITDIR/install-folders.sh $@
-$INITDIR/install-init.sh $@
+$INITDIR/init-folders.sh $@
+$INITDIR/init-rc.sh $@
