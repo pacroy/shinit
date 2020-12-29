3da3bd46614d18365451d469bbec090fafd8faf7
Par <pacroy@gmail.com>
2020-12-29 14:59:15 +0700

    Fix chmod

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ INITDIR="$HOME/.init"
 git --version
 if [ -d "$INITDIR" ]; then
     CURRENTDIR=$(pwd)
-    cd "$INITDIR" && git pull
+    cd "$INITDIR"
+    git pull
     cd $CURRENTDIR
     unset CURRENTDIR
 else
     git clone --depth 1 https://github.com/pacroy/sh-init.git "$INITDIR"
 fi
-chmod +x "$INITDIR/init.sh"
+chmod +x $INITDIR/*.sh
 $INITDIR/init.sh
