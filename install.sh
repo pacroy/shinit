dbb31b9d30a365fa0d9cc9fa352fee2259957aa2
Par <pacroy@gmail.com>
2020-12-29 14:37:20 +0700

    Fix wrong init script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ INITDIR="$HOME/.init"
 
 git --version
 git clone --depth 1 https://github.com/pacroy/sh-init.git "$INITDIR"
-chmod +x "$INITDIR/install.sh"
-$INITDIR/install.sh
+chmod +x "$INITDIR/init.sh"
+$INITDIR/init.sh
