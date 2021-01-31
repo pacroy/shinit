c9d115026c1730c9982b5ebe8fdd18aaa5f3a3ac
Par <pacroy@gmail.com>
2021-01-31 11:57:39 +0700

    Include upgrade pip

--- install-python.sh
+++ install-python.sh
@@ -9,6 +9,7 @@ sudo apt install -y python3 python3-pip
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 1
 sudo update-alternatives --display python
 python --version
+python -m pip install -U pip
 sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
 sudo update-alternatives --display pip
-pip --version
+python -m pip --version
