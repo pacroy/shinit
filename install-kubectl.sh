226b30aa06c05ac601ef651d6d217b8b07b6dbc1
Par <pacroy@gmail.com>
2021-04-14 15:55:18 +0700

    Change shell to bash

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # References:
 # - https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
 
--- install-python.sh
+++ install-python.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # References
 # - https://github.com/pyenv/pyenv
 # - https://github.com/pyenv/pyenv-installer
