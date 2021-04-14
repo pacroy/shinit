7f49d0bda80c42a63ea7afb89ba87364d7b5d098
Par <pacroy@gmail.com>
2021-04-14 15:44:31 +0700

    Use $HOME as cache

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -6,8 +6,11 @@ set -o errexit
 set -o pipefail
 
 # Download the latest version
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o $HOME/kubectl
 
 # Install
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 $HOME/kubectl /usr/local/bin/kubectl
 kubectl version --client
+
+# Clean up
+rm $HOME/kubectl
