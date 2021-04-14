6c8926338d06ca7bca5766bd487bfdec52dc719e
Par <pacroy@gmail.com>
2021-04-14 15:36:27 +0700

    Install Python with pyenv instead "apt install"

--- install-python.sh
+++ install-python.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
-# Source: https://phoenixnap.com/kb/how-to-install-python-3-ubuntu
-#         https://hackersandslackers.com/multiple-versions-python-ubuntu
-#         https://phoenixnap.com/kb/how-to-install-pip-on-ubuntu
-#         https://pip.pypa.io/en/stable/installing/
-set -e
+# References
+# - https://github.com/pyenv/pyenv
+# - https://github.com/pyenv/pyenv-installer
+# - https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+# - https://github.com/momo-lab/xxenv-latest
 
-sudo apt update
-sudo apt install -y python3 python3-pip
-sudo update-alternatives --install /usr/bin/python python $(readlink -f $(command -v python3)) 1
-sudo update-alternatives --display python
+set -o errexit
+set -o pipefail
+
+# Install pyenv
+curl https://pyenv.run | bash
+
+# Configure .bashrc
+printf '\nexport PATH="/home/pacroy/.pyenv/bin:$PATH"\n' >> $HOME/.bashrc
+printf 'eval "$(pyenv init -)"\n' >> $HOME/.bashrc
+printf 'eval "eval$(pyenv virtualenv-init -)"\n' >> $HOME/.bashrc
+
+# Configure .zshrc
+printf '\nexport PATH="/home/pacroy/.pyenv/bin:$PATH"\n' >> $HOME/.zshrc
+printf 'eval "$(pyenv init -)"\n' >> $HOME/.zshrc
+printf 'eval "eval$(pyenv virtualenv-init -)"\n' >> $HOME/.zshrc
+
+# Install build dependencies
+sudo apt-get update; sudo apt-get install --yes --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+# Install xxenv-latest
+git clone https://github.com/momo-lab/xxenv-latest.git "$(pyenv root)"/plugins/xxenv-latest
+
+# Install the latest Python 3
+pyenv latest install
+
+# Set global python to the latest version
+pyenv latest global
 python --version
-python -m pip install -U pip
-sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
-sudo update-alternatives --display pip
-python -m pip --version
