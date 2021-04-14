17ba19ca769f24b1d22d3e9e82bf81c70ebb1a70
Par <pacroy@gmail.com>
2021-04-14 15:07:46 +0700

    Add static init.sh script

--- init-rc.sh
+++ init-rc.sh
@@ -5,24 +5,6 @@ INITDIR="$1"
 if [ -z $INITDIR ]; then echo 'INITDIR is not specified' >&2 && exit 1; fi
 
 INITSH="$INITDIR/init.sh"
-ALIASES="$INITDIR/aliases.sh"
-BINDIR="$HOME/bin"
-
-echo '#!/bin/sh' > "$INITSH"
-echo 'INITDIR="$HOME/.init"' > "$INITSH"
-
-# Add aliases
-if [ -f "$ALIASES" ]; then
-    echo "source $ALIASES" >> "$INITSH"
-fi
-
-# Add bin directory to PATH
-if [ -d "$BINDIR" ]; then
-    echo 'PATH=$PATH:'"$BINDIR" >> "$INITSH"
-fi
-
-# Configure less behavior
-echo 'unset LESS' >> "$INITSH"
 
 # Configure .bashrc or .bash_profile
 if [ -f "$HOME/.bashrc" ]; then
---
+++ init.sh
@@ -0,0 +1,4 @@
+INITDIR="$HOME/.init"
+source $HOME/.init/aliases.sh
+PATH=$PATH:$HOME/bin
+unset LESS
