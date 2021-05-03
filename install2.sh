56abfb7b9fd6f9975d727ff5877befd6765668cb
Par <pacroy@gmail.com>
2021-05-03 19:29:21 +0700

    Fix linting issue in install2.sh

--- install2.sh
+++ install2.sh
@@ -2,12 +2,12 @@
 set -e
 
 INITDIR="${1}"
-if [ -z $INITDIR ]; then echo 'INITDIR is not specified' >&2 && exit 1; fi
+if [ -z "${INITDIR}" ]; then echo 'INITDIR is not specified' >&2 && exit 1; fi
 
-$INITDIR/init-dirs.sh $@
-$INITDIR/init-ssh.sh $@
-$INITDIR/init-git.sh $@
+"${INITDIR}/init-dirs.sh" "$@"
+"${INITDIR}/init-ssh.sh" "$@"
+"${INITDIR}/init-git.sh" "$@"
 
-$INITDIR/init-rc.sh $@
+"${INITDIR}/init-rc.sh" "$@"
 
 echo "Installation completed"
