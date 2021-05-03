bf7d971effbd4c778602f984109a9e13f2c24f01
Par <pacroy@gmail.com>
2021-05-03 15:09:14 +0700

    Update install-jq using bash

--- install-jq.sh
+++ install-jq.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Source: https://stedolan.github.io/jq/download/
-set -e
+set -o errexit
+set -o pipefail
 
-curl -fL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o $HOME/bin/jq
-chmod +x $HOME/bin/jq
+JQ_VERSION="1.6"
+
+# Download
+curl -fL "https://github.com/stedolan/jq/releases/download/jq-${JQ_VERSION}/jq-linux64" -o "${HOME}/jq"
+
+# Install
+sudo install -o root -g root -m 0755 "${HOME}/jq" /usr/local/bin/oc
 jq --version
+
+# Clean up
+rm "${HOME}/jq"
