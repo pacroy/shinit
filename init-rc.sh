2238e32fb36bc3807be8b2cf905e5d9a02cfd797
Par <pacroy@gmail.com>
2021-05-03 19:20:25 +0700

    Fix linting issues in init-rc

--- init-rc.sh
+++ init-rc.sh
@@ -2,24 +2,24 @@
 set -e
 
 INITDIR="${1}"
-if [ -z $INITDIR ]; then echo 'INITDIR is not specified' >&2 && exit 1; fi
+if [ -z "${INITDIR}" ]; then echo 'INITDIR is not specified' >&2 && exit 1; fi
 
 INITSH="$INITDIR/init.sh"
 
 # Configure .bashrc or .bash_profile
-if [ -f "$HOME/.bashrc" ]; then
-    if ! cat $HOME/.bashrc | grep "source $INITSH" > /dev/null; then
-        echo "\nsource $INITSH" >> $HOME/.bashrc
+if [ -f "${HOME}/.bashrc" ]; then
+    if ! grep -F "source ${INITSH}" "${HOME}/.bashrc" > /dev/null; then
+        echo "\nsource ${INITSH}" >> "${HOME}/.bashrc"
     fi
-elif [ -f "$HOME/.bash_profile" ]; then
-    if ! cat $HOME/.bash_profile | grep "source $INITSH" > /dev/null; then
-        echo "\nsource $INITSH" >> $HOME/.bash_profile
+elif [ -f "${HOME}/.bash_profile" ]; then
+    if ! grep -F "source ${INITSH}" "${HOME}/.bash_profile" > /dev/null; then
+        echo "\nsource ${INITSH}" >> "${HOME}/.bash_profile"
     fi
 fi
 
 # Configure .zshrc
-if [ -f "$HOME/.zshrc" ]; then
-    if ! cat $HOME/.zshrc | grep "source $INITSH" > /dev/null; then
-        echo "\nsource $INITSH" >> $HOME/.zshrc
+if [ -f "${HOME}/.zshrc" ]; then
+    if ! grep -F "source ${INITSH}" "${HOME}/.zshrc" > /dev/null; then
+        echo "\nsource ${INITSH}" >> "${HOME}/.zshrc"
     fi
 fi
