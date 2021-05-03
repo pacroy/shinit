0e6689805aa02b48b61c05cbbd802c809eefef5c
Par <pacroy@gmail.com>
2021-05-03 15:14:57 +0700

    Double quote variable

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -6,11 +6,11 @@ set -o errexit
 set -o pipefail
 
 # Download the latest version
-curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o $HOME/kubectl
+curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o "${HOME}/kubectl"
 
 # Install
-sudo install -o root -g root -m 0755 $HOME/kubectl /usr/local/bin/kubectl
+sudo install -o root -g root -m 0755 "${HOME}/kubectl" /usr/local/bin/kubectl
 kubectl version --client
 
 # Clean up
-rm $HOME/kubectl
+rm "${HOME}/kubectl"
