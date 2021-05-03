dd7f56ee4d6d8f21c2d5aa353ad45b79b0fad5c3
Par <pacroy@gmail.com>
2021-05-03 19:22:54 +0700

    Fix linting issue in init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
-INITDIR="$HOME/.init"
-source $HOME/.init/aliases.sh
-PATH=$PATH:$HOME/bin
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+INITDIR="${HOME}/.init"
+source "${HOME}/.init/aliases.sh"
+PATH="${PATH}:${HOME}/bin"
 unset LESS
