e47f21837ae43254b894fccc9c7e9ead93d71b1b
Par <chairat.onyaem@exxonmobil.com>
2021-05-03 15:37:34 +0700

    Update install-terraform to use bash

--- install-terraform.sh
+++ install-terraform.sh
@@ -1,9 +1,25 @@
-#!/bin/sh
-# Source: https://www.terraform.io/downloads.html
-set -e
+#!/usr/bin/env bash
+# References:
+# - https://www.terraform.io/downloads.html
 
-curl -fL https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip -o $HOME/bin/terraform.zip
-sudo apt-get install -y zip unzip
-unzip $HOME/bin/terraform.zip -d $HOME/bin
-rm $HOME/bin/terraform.zip
+set -o errexit
+set -o pipefail
+
+TERRAFORM_VERSION="0.15.1"
+
+# Check unzip
+if ! which unzip; then
+    sudo apt install -y unzip
+fi
+
+# Download
+curl -fL "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -o "${HOME}/terraform.zip"
+unzip -j "${HOME}/kubelogin.zip" terraform -d "${HOME}"
+
+# Install
+sudo install -o root -g root -m 0755 "${HOME}/terraform" /usr/local/bin/terraform
 terraform version
+
+# Clean up
+rm "${HOME}/terraform"
+rm "${HOME}/terraform.zip"
