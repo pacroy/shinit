16eacdba9b5b84ae084813275b136bf4e026ab2d
Par <pacroy@gmail.com>
2021-05-17 13:47:20 +0700

    Check directory existence before creating it

--- init-dirs.sh
+++ init-dirs.sh
@@ -2,14 +2,20 @@
 set -o errexit
 set -o pipefail
 
-echo "Creating directory ~/dev (if not exist) ..."
-mkdir -p ~/dev
-echo "Creating directory ~/bin (if not exist) ..."
-mkdir -p ~/bin; 
+if [ ! -d ~/dev ]; then
+    echo "Creating directory ~/dev (if not exist) ..."
+    mkdir -p ~/dev
+fi
+if [ ! -d ~/bin ]; then
+    echo "Creating directory ~/bin (if not exist) ..."
+    mkdir -p ~/bin;
+fi 
 
 username=$(whoami)
-echo "Creating symbolic link ~/${username} ..."
-usrdir_default="/mnt/c/Users/${username}/dev"
-read -rp "Home path on host [ENTER for ${usrdir_default}]:" usrdir_input </dev/tty
-usr_path=${usrdir_input:-${usrdir_default}}
-ln -s "${usr_path}" "~/${username}"
+if [ ! -d "~/${username}" ]; then
+    echo "Creating symbolic link ~/${username} ..."
+    usrdir_default="/mnt/c/Users/${username}/dev"
+    read -rp "Home path on host [ENTER for ${usrdir_default}]:" usrdir_input </dev/tty
+    usr_path=${usrdir_input:-${usrdir_default}}
+    ln -sf "${usr_path}" "~/${username}"
+fi
