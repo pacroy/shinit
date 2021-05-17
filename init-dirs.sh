7ac0b227b56c29bfcd7c4b62dbefbdffacb2d9e2
Par <pacroy@gmail.com>
2021-05-17 11:34:10 +0700

    Simplify creating directories

--- init-dirs.sh
+++ init-dirs.sh
@@ -2,20 +2,7 @@
 set -o errexit
 set -o pipefail
 
-if [ ! -d ~/dev ]; then 
-    if [ -z "${WSLENV}" ]; then
-        echo "Creating directory ~/dev ..."
-        mkdir ~/dev
-    else
-        devdir_default="/mnt/c/Users/$(whoami)/dev"
-        read -rp "dev path [ENTER for ${devdir_default}]:" devdir_input </dev/tty
-        dev_path=${devdir_input:-${devdir_default}}
-        mkdir -p "${dev_path}"
-        ln -s "${dev_path}" ~/dev
-    fi
-fi
-
-if [ ! -d ~/bin ]; then 
-    echo "Creating directory ~/bin ..."
-    mkdir ~/bin; 
-fi
+echo "Creating directory ~/dev (if not exist) ..."
+mkdir -p ~/dev
+echo "Creating directory ~/bin (if not exist) ..."
+mkdir -p ~/bin; 
