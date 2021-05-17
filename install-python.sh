bb375213eaead0421544595946662513f8581a5c
Par <pacroy@gmail.com>
2021-05-17 10:23:27 +0700

    Use raw link instead

--- install-python.sh
+++ install-python.sh
@@ -9,7 +9,7 @@ set -o errexit
 set -o pipefail
 
 # Install pyenv
-curl https://pyenv.run | bash
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
 # Configure .bashrc
 {
