d293cf1a9bd49b109802ea735606305cf7ecd035
Par <pacroy@gmail.com>
2021-05-17 11:37:06 +0700

    Add host's home directory

--- init-dirs.sh
+++ init-dirs.sh
@@ -6,3 +6,10 @@ echo "Creating directory ~/dev (if not exist) ..."
 mkdir -p ~/dev
 echo "Creating directory ~/bin (if not exist) ..."
 mkdir -p ~/bin; 
+
+username=$(whoami)
+echo "Creating symbolic link ~/${username} ..."
+usrdir_default="/mnt/c/Users/${username}/dev"
+read -rp "Home path on host [ENTER for ${usrdir_default}]:" usrdir_input </dev/tty
+usr_path=${usrdir_input:-${usrdir_default}}
+ln -s "${usr_path}" "~/${username}"
