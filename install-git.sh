da29f1a49cd8fe8b4163308d42f454929d74b4ea
Par <pacroy@gmail.com>
2021-06-06 13:41:23 +0700

    Update shebang to bash

--- install-git.sh
+++ install-git.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Source: http://lifeonubuntu.com/upgrading-ubuntu-to-use-the-latest-git-version/
-set -e
+set -o errexit
+set -o pipefail
 
 sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt-get update
--- install2.sh
+++ install2.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-set -e
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
 
 INITDIR="${1}"
 if [ -z "${INITDIR}" ]; then echo 'INITDIR is not specified' >&2 && exit 1; fi
