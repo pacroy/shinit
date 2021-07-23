104feadf0ff9a4fb6f6c4b1e198bee596878bc42
Par <pacroy@gmail.com>
2021-07-23 15:11:12 +0700

    Download latest version of terraform

--- install/terraform.sh
+++ install/terraform.sh
@@ -5,14 +5,25 @@
 set -o errexit
 set -o pipefail
 
-TERRAFORM_VERSION="0.15.1"
+# Check and install required softwares
+softwares=""
+which unzip > /dev/null || softwares+="unzip "
+which jq > /dev/null || softwares+="jq "
+if [ -n "$softwares" ]; then
+    sudo apt update
+    sudo apt install -y "$softwares"
+fi
 
-# Check unzip
-if ! which unzip; then
-    sudo apt install -y unzip
+# Check input variables
+if [ -z "${TERRAFORM_VERSION}" ]; then
+	terraform_latest_tag=$(curl -fsSL https://api.github.com/repos/hashicorp/terraform/releases/latest | jq -r ".tag_name")
+	terraform_latest_version=${terraform_latest_tag#"v"}
+	TERRAFORM_VERSION="$terraform_latest_version"
 fi
 
 # Download
+url="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" 
+echo "Downloading from $url ..."
 curl -fL "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -o "${HOME}/terraform.zip"
 unzip -j "${HOME}/terraform.zip" terraform -d "${HOME}"
 
@@ -23,3 +34,4 @@ terraform version
 # Clean up
 rm "${HOME}/terraform"
 rm "${HOME}/terraform.zip"
+
