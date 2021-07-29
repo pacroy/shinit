bd8470fb6f9fc39c9dfe7d535531ec11097cb87c
Par <pacroy@gmail.com>
2021-07-29 15:37:51 +0700

    Fix not a git repo when install

--- install.sh
+++ install.sh
@@ -12,8 +12,11 @@ if [ -d "${INITDIR}" ]; then
     )
 else
     git clone --quiet --depth 1 https://github.com/pacroy/sh-init.git "${INITDIR}"
-    git submodule init
-    git submodule update
+    {
+        cd "${INITDIR}"
+        git submodule init
+        git submodule update
+    }
 fi
 
 "${INITDIR}/install2.sh" "${INITDIR}"
