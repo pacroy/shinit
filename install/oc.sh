d3657d1cf3d46302d803013f9e58d556bd9d093d
Par <pacroy@gmail.com>
2021-08-05 13:19:36 +0700

    Remove oc3

--- install/oc.sh
+++ install/oc.sh
@@ -3,32 +3,16 @@
 set -o errexit
 set -o pipefail
 
-OC_VERSION="3.11.420-1"
-OC_URL="https://mirror.openshift.com/pub/openshift-v3/clients/${OC_VERSION}/linux/oc.tar.gz"
-
 OC4_VERSION="4.6.29"
 OC4_URL="https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/${OC4_VERSION}/openshift-client-linux.tar.gz"
-
-# Download oc3
-curl -fL "${OC_URL}" -o "${HOME}/oc.tgz"
-tar -C "${HOME}" -xzf "${HOME}/oc.tgz" oc
-
-# Install oc3
-sudo install -o root -g root -m 0755 "${HOME}/oc" /usr/local/bin/oc
-sudo ln -s /usr/local/bin/oc /usr/local/bin/oc3
-oc version
-
-# Clean up oc3
-rm "${HOME}/oc"
-rm "${HOME}/oc.tgz"
 
 # Download oc4
 curl -fL "${OC4_URL}" -o "${HOME}/oc4.tgz"
 tar -C "${HOME}" -xzf "${HOME}/oc4.tgz" oc
 
 # Install oc4
-sudo install -o root -g root -m 0755 "${HOME}/oc" /usr/local/bin/oc4
-oc4 version
+sudo install -o root -g root -m 0755 "${HOME}/oc" /usr/local/bin/oc
+oc version
 
 # Clean up oc4
 rm "${HOME}/oc"
