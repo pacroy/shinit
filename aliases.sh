7f532eb6b266c7c81cef5bf4eed43463cf96bb7f
Chairat Onyaem (Par) <pacroy@gmail.com>
2022-05-17 13:24:37 +0700

    Change relative date (ar) to short one (as) (#9)
    * Change relative date (ar) to short one (as)
    
    * Remove parentheses over dates

--- aliases.sh
+++ aliases.sh
@@ -70,7 +70,7 @@ alias tg='terragrunt'
 
 # Git
 alias gitl='git log'
-alias gitl1="git log --oneline --decorate --format=format:'%C(blue)%h%C(reset) %C(green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(yellow)%d%C(reset)'"
+alias gitl1="git log --oneline --decorate --format=format:'%C(blue)%h%C(reset) %C(green)%as%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(yellow)%d%C(reset)'"
 # git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all
 alias gitt='git tag'
 alias gittl='git tag --list -n1 --sort=-tag'
