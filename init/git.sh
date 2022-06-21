20b99bce8ee6a0eb217c70d26542006d56470373
Par <pacroy@gmail.com>
2022-06-21 20:29:14 +0700

    Use git config --get instead

--- init/git.sh
+++ init/git.sh
@@ -2,27 +2,27 @@
 set -o errexit
 set -o pipefail
 
-if [ -z "$(git config --global user.name)" ]; then 
+if [ -z "$(git config --get user.name)" ]; then 
     read -rp "Your git name : " git_name </dev/tty
-    git config --global user.name "$git_name"
+    git config --get user.name "$git_name"
 fi
 
-if [ -z "$(git config --global user.email)" ]; then
+if [ -z "$(git config --get user.email)" ]; then
     read -rp "Your git email: " git_email </dev/tty
-    git config --global user.email "${git_email}"
+    git config --get user.email "${git_email}"
 fi
 
-if [ -z "$(git config --global push.default)" ]; then
+if [ -z "$(git config --get push.default)" ]; then
     echo "Setting git push.default=simple ..."
-    git config --global push.default simple
+    git config --get push.default simple
 fi
 
-if [ -z "$(git config --global pull.rebase)" ]; then
+if [ -z "$(git config --get pull.rebase)" ]; then
     echo "Setting git pull.rebase=false ..."
-    git config --global pull.rebase false
+    git config --get pull.rebase false
 fi
 
-if [ -z "$(git config --global init.defaultBranch)" ]; then
+if [ -z "$(git config --get init.defaultBranch)" ]; then
     echo "Setting git init.defaultBranch=main ..."
-    git config --global init.defaultBranch main
+    git config --get init.defaultBranch main
 fi
