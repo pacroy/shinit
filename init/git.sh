f09423049f9ebd4c108b677a1046c7387ecbf6da
Par <pacroy@users.noreply.github.com>
2022-07-04 20:21:32 +0700

    Add create PGP block

--- init/git.sh
+++ init/git.sh
@@ -19,3 +19,30 @@ if [ -z "$(git config --get init.defaultBranch)" ]; then
     echo "Configuring git init.defaultBranch=main ..."
     git config --get init.defaultBranch main
 fi
+
+if [ -z "$(git config --get user.signingkey)" ]; then
+    echo "A new GPG will be generate to sign your git commits. Please choose the following options when asked:"
+    echo "   kind of key = RSA and RSA"
+    echo "   keysize = 4096"
+    echo "   how long the key should be valid = <as you want e.g. 1y>"
+    echo "   Real name = Your full name"
+    echo "   Email address = <github_username>\@users.noreply.github.com"
+    echo "   Comment = <as you want e.g. username\@hotname>
+    echo ""
+    echo "Please [Enter] key to continue..." && read -p "$*"
+
+    gpg --full-generate-key
+    gpg --list-secret-keys --keyid-format=long
+
+    echo ""
+    read -rp "GPG key ID : " pgp_key_id </dev/tty
+    echo "Configuring git user.signingkey=${pgp_key_id} ..."
+    git config --get user.signingkey "${pgp_key_id}"
+    echo "Configuring git commit.gpgsign=true ..."
+    git config --global commit.gpgsign true
+    echo "Configuring git tag.gpgSign=true ..."
+    git config --global tag.gpgSign true
+
+    echo "Please add the following GPG public key in your GitHub settings:"
+    gpg --armor --export "${pgp_key_id}"
+fi
