218a469643c0157fd547945d85b9207dee5ef0d3
Par <pacroy@users.noreply.github.com>
2022-07-04 19:59:00 +0700

    Use private email

--- init/git.sh
+++ init/git.sh
@@ -3,16 +3,19 @@ set -o errexit
 set -o pipefail
 
 if [ -z "$(git config --get user.name)" ]; then 
-    read -rp "Your git name : " git_name </dev/tty
+    read -rp "Your name : " git_name </dev/tty
+    echo "Configuring git user.name=${git_name} ..."
     git config --get user.name "$git_name"
 fi
 
 if [ -z "$(git config --get user.email)" ]; then
-    read -rp "Your git email: " git_email </dev/tty
+    read -rp "Your GitHub username: " github_username </dev/tty
+    git_email="${github_username@users.noreply.github.com"
+    echo "Configuring git user.email=${git_email} ..."
     git config --get user.email "${git_email}"
 fi
 
 if [ -z "$(git config --get init.defaultBranch)" ]; then
-    echo "Setting git init.defaultBranch=main ..."
+    echo "Configuring git init.defaultBranch=main ..."
     git config --get init.defaultBranch main
 fi
