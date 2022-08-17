7ee6322abff40186a8b34d3fb704cecb6243e69b
Chairat Onyaem (Par) <pacroy@users.noreply.github.com>
2022-08-17 18:55:12 +0700

    Prompt to generate GPG/SSH key (#20)

--- init/ssh.sh
+++ init/ssh.sh
@@ -3,10 +3,13 @@ set -o errexit
 set -o pipefail
 
 if [ ! -d ~/.ssh ] || ! ls ~/.ssh/id* &>/dev/null; then
-    echo "Generate a new SSH keypair..."
-    ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+    read -rp "SSH key is not found. Generate and configure a new one? (yes to proceed): " is_generate_ssh </dev/tty
+    if [ "${is_generate_ssh}" = "yes" ]; then
+        echo "Generate a new SSH keypair..."
+        ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 
-    echo "Adding the new SSH keypair to SSH agent..."
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_rsa
+        echo "Adding the new SSH keypair to SSH agent..."
+        eval "$(ssh-agent -s)"
+        ssh-add ~/.ssh/id_rsa
+    fi
 fi
