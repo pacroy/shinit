41d5351bad2fd0f00d60765530038c2b4c8616f7
Chairat Onyaem (Par) <pacroy@users.noreply.github.com>
2022-09-06 20:51:28 +0700

    Fix read being skipped in git bash (#22)
    Add missing </dev/tty for read

--- init/git.sh
+++ init/git.sh
@@ -31,7 +31,7 @@ if [ -z "$(git config --get user.signingkey)" ]; then
         echo "   Email address = <github_username>@users.noreply.github.com"
         echo "   Comment = <as you want e.g. username@hotname>"
         echo ""
-        printf "Please [Enter] key to continue..." && read -r
+        printf "Please [Enter] key to continue..." && read -r </dev/tty
 
         gpg --full-generate-key
         gpg --list-secret-keys --keyid-format=long
