8554c86ee242a74364cead0db4fe75075ebcb92a
Par <pacroy@users.noreply.github.com>
2022-09-06 20:37:15 +0700

    Fix git config set commands

--- init/git.sh
+++ init/git.sh
@@ -5,19 +5,19 @@ set -o pipefail
 if [ -z "$(git config --get user.name)" ]; then 
     read -rp "Your name : " git_name </dev/tty
     echo "Configuring git user.name=${git_name} ..."
-    git config --get user.name "$git_name"
+    git config --global user.name "$git_name"
 fi
 
 if [ -z "$(git config --get user.email)" ]; then
     read -rp "Your GitHub username: " github_username </dev/tty
     git_email="${github_username}@users.noreply.github.com"
     echo "Configuring git user.email=${git_email} ..."
-    git config --get user.email "${git_email}"
+    git config --global user.email "${git_email}"
 fi
 
 if [ -z "$(git config --get init.defaultBranch)" ]; then
     echo "Configuring git init.defaultBranch=main ..."
-    git config --get init.defaultBranch main
+    git config --global init.defaultBranch main
 fi
 
 if [ -z "$(git config --get user.signingkey)" ]; then
